c2cbb571564090eae6be008426cd0399833fe65d
Yslas <jryslas@gmail.com>
2018-11-12 17:17:40 -0800

    no node modules not attached, files started.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.!!!!!!!!!!!!  Line 2 purposefully dosen't have a boolean to test the default!!!!!!!!!1
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Jalepeno Burger', false);
+INSERT INTO burgers (name) VALUES ('Seitan Burger'false);
